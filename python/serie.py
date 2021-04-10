8b8b63de4a351b0e8b491f28aaaee5e2a2df9251
arthurmio <arthurmio@protonmail.com>
2021-04-10 18:13:04 +0200

    Plot in real time corrected

--- python/serie.py
+++ python/serie.py
@@ -1,21 +1,29 @@
+# -------------------------------------------------------
+# -------------------------------------------------------
+# Projet : Walking Analysis Project
+# Titre  : Affichage courbe en temps réel
+# Auteur : Mathéo Gourdon - Arthur Mariano - Ianis Trigui
+# Date   : 10/04/2021
+# Github : arthurmio
+# Python : version 3.9.2
+# -------------------------------------------------------
+# -------------------------------------------------------
 import time
 import serial
 import matplotlib.pyplot as plt
 from itertools import count
+from serie import *
 
 def serialConnect():
     fig = plt.figure()
-    #ax = fig.add_subplot(111) # à modifier?
+    ax = fig.add_subplot(111)
     fig.show()
-    
-    x_vals1 = [] # arduino 1
-    x_vals2 = [] # arduino 2
 
-    y_vals1 = [] # Yaw
-    y_vals2 = [] # Pitch
-    y_vals3 = [] # Roll
+    x_vals  = []
+    y_vals1 = []
+    y_vals2 = []
+    y_vals3 = []
     index = count()
-
     # lecture du port série
     arduinoData=serial.Serial('COM4', 115200)
     time.sleep(1)
@@ -23,33 +31,25 @@ def serialConnect():
         while (arduinoData.inWaiting()==0) :
             pass
         # lecture du yaw/pitch/roll de l'arduino
-        dataPacket  = arduinoData.readline()
-        dataPacket  = str(dataPacket,'utf-8')
-        splitPacket = dataPacket.split(',')
-        index = float(splitPacket[0])
-        Yaw   = float(splitPacket[1])
-        Pitch = float(splitPacket[2])
-        Roll  = float(splitPacket[3])
-        print("index: ",index,"Yaw: ",Yaw," Pitch:",Pitch," Roll:",Roll)
-        if(index==1): # arduino1
-            x_vals1.append(next(index))
-            y_vals1.append(Yaw)
-            y_vals2.append(Pitch)
-            y_vals3.append(Roll)
-            ax.plot(x_vals1, y_vals1, color='b', linewidth=1)
-            ax.plot(x_vals1, y_vals2, color='r', linewidth=1)
-            ax.plot(x_vals1, y_vals3, color='green', linewidth=1)
-            ax.subplot(2,1,1)
-            fig.canvas.draw()
-        elif(index==2): # arduino2
-            x_vals2.append(next(index))
-            y_vals1.append(Yaw)
-            y_vals2.append(Pitch)
-            y_vals3.append(Roll)
-            ax.plot(x_vals2, y_vals1, color='b', linewidth=1)
-            ax.plot(x_vals2, y_vals2, color='r', linewidth=1)
-            ax.plot(x_vals2, y_vals3, color='green', linewidth=1)
-            ax.subplot(2,1,2)
-            fig.canvas.draw()
+        dataPacket=arduinoData.readline()
+        dataPacket= str(dataPacket,'utf-8')
+        splitPacket=dataPacket.split(',')
+        Yaw=float(splitPacket[2])
+        Pitch=float(splitPacket[3])
+        Roll=float(splitPacket[4])
+        print("Yaw: ",Yaw," Pitch:",Pitch," Roll:",Roll)
 
-serialConnect()
+        # mise des valeurs du yaw/pitch/roll sur le graphique
+        x_vals.append(next(index))
+        y_vals1.append(Yaw)
+        y_vals2.append(Pitch)
+        y_vals3.append(Roll)
+        ax.grid()
+        ax.set_title("Affichage temps réel")
+        ax.plot(x_vals, y_vals1, color='b')
+        ax.plot(x_vals, y_vals2, color='r')
+        ax.plot(x_vals, y_vals3, color='g')
+        fig.canvas.draw()
+
+
+serialConnect() 
